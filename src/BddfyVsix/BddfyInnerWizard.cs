0752d3782d3a34c5a4296e72f82e126957feae4f
David Hsieh <dcchsieh@gmail.com>
2016-03-18 18:35:47 +1000

    Now multiple Assertion and Arrangements are now reflected in the class template.

--- src/BddfyVsix/BddfyInnerWizard.cs
+++ src/BddfyVsix/BddfyInnerWizard.cs
@@ -2,6 +2,7 @@
 using EnvDTE;
 using Microsoft.VisualStudio.TemplateWizard;
 using System.Linq;
+using System.Reflection.Emit;
 
 namespace BddfyVsix
 {
@@ -32,19 +33,82 @@ namespace BddfyVsix
             replacementsDictionary.Add("$StoryIWant$", form.StoryIWantTextBox.Text);
             replacementsDictionary.Add("$StorySoThat$", form.StorySoThatTextBox.Text);
 
-            replacementsDictionary.Add("$MainArrangementSection$", string.Format("public void {0}() {{ \n }}", form.MainArrangementTextBox.Items[0]));
+            #region Arrangement Section
+            if (form.MainArrangementTextBox.Items.Count == 1)
+            { 
+                replacementsDictionary.Add("$MainArrangementSection$", string.Format("public void {0}() {{ \n }}", form.MainArrangementTextBox.Items[0]));
+                replacementsDictionary.Add("$MainArrangementStatements$", string.Format("this.Given(_ => {0}())", form.MainArrangementTextBox.Items[0]));
+            } 
+            else
+            {
+                var index = 0;
+                var mainArrangementSectionReplacementString = string.Empty;
+                var mainArrangementStatementsReplacementString = string.Empty;
+                foreach(var i in form.MainArrangementTextBox.Items)
+                {
+                    mainArrangementSectionReplacementString += string.Format("public void {0}() {{ \n }} \n\n", i);
+                    if (index == 0)
+                    {
+                        mainArrangementStatementsReplacementString += string.Format("this.Given(_ => {0}())\n", i);
+                    }
+                    else if (index == form.MainArrangementTextBox.Items.Count -1)
+                    {
+                        mainArrangementStatementsReplacementString += string.Format("\t\t\t\t.And(_ => {0}())", i);
+                    }
+                    else
+                    {
+                        mainArrangementStatementsReplacementString += string.Format("\t\t\t\t.And(_ => {0}())\n", i);
+                    }
+
+                    index++;
+                }
+                replacementsDictionary.Add("$MainArrangementSection$", mainArrangementSectionReplacementString);
+                replacementsDictionary.Add("$MainArrangementStatements$", mainArrangementStatementsReplacementString);
+
+            }
+            #endregion
+
             replacementsDictionary.Add("$MainActSection$", string.Format("public void {0}() {{ \n }}", form.MainActionItemTextBox.Text));
-            replacementsDictionary.Add("$MainAssertionSection$", string.Format("public void {0}() {{ \n }}", form.MainAssertionTextBox.Items[0]));
+
+            #region Assertion Section
+            if (form.MainAssertionTextBox.Items.Count == 1)
+            {
+                replacementsDictionary.Add("$MainAssertionSection$", string.Format("public void {0}() {{ \n }}", form.MainAssertionTextBox.Items[0]));
+                replacementsDictionary.Add("$MainAssertionStatements$", string.Format(".Then(_ => {0}())", form.MainAssertionTextBox.Items[0]));
+            }
+            else
+            {
+                var index = 0;
+                var mainAssertionSectionReplacementString = string.Empty;
+                var mainAssertionStatementsReplacementString = string.Empty;
+                foreach (var i in form.MainAssertionTextBox.Items)
+                {
+                    mainAssertionSectionReplacementString += string.Format("public void {0}() {{ \n }} \n\n", i);
+                    if (index == 0)
+                    {
+                        mainAssertionStatementsReplacementString += string.Format(".Then(_ => {0}())\n", i);
+                    }
+                    else if (index == form.MainAssertionTextBox.Items.Count -1)
+                    {
+                        mainAssertionStatementsReplacementString += string.Format("\t\t\t\t.And(_ => {0}())", i);
+                    }
+                    else
+                    {
+                        mainAssertionStatementsReplacementString += string.Format("\t\t\t\t.And(_ => {0}())\n", i);
+                    }
+                    index++;
+                }
+                replacementsDictionary.Add("$MainAssertionSection$", mainAssertionSectionReplacementString);
+                replacementsDictionary.Add("$MainAssertionStatements$", mainAssertionStatementsReplacementString);
+            }
+            #endregion
 
             replacementsDictionary.Add("$MainScenarioMethod$", form.MainScenarioMethodTextBox.Text);
             //TODO: perhap use Humanizr to format this?
 
-            replacementsDictionary.Add("$MainArrangementStatements$", string.Format("this.Given(_ => {0}())", form.MainArrangementTextBox.Items[0]));
             replacementsDictionary.Add("$MainAct$", form.MainActionItemTextBox.Text);
-            replacementsDictionary.Add("$MainAssertionStatements$", string.Format(".Then(_ => {0}())", form.MainAssertionTextBox.Items[0]));
             replacementsDictionary.Add("$ScenarioTitle$", form.ScenarioTitleTextBox.Text);
 
-            //replacementsDictionary.Add("$MainAssertion$", form.MainAssertionTextBox.Text);
             var baseClassChecked = form.IsUseBaseTest.Checked;
             if (baseClassChecked)
             {
